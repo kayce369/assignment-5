e1b367d14223a52cf1bcd095eb5048c1c706e3d7
Kayce Georgi <georgi.kayce@ufl.edu>
2021-04-17 21:36:09 -0400

    programmed form, fixed errors

---
+++ form/default.inc.php
@@ -0,0 +1,52 @@
+<style type="text/css">
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+body {
+  max-width: 1500px;
+  min-width: 1225px;
+  margin: auto;
+  height: 100vh;
+  background: #F1F1F1;
+}
+
+h1 {
+  font-family: 'Source Code Pro', monospace;
+  font-size: 36pt;
+  padding: 20px 0 20px 100px;
+}
+
+p {
+  font-family: 'Lato', sans-serif;
+  font-size: 14pt;
+}
+
+a {
+  color: #0021A5;
+  text-decoration: none;
+  transition: .3s;
+}
+
+a:hover {
+  text-decoration: underline 2;
+  text-decoration-color: #0021A5;
+  transition: .3s;
+}
+
+a:active {
+  text-decoration: underline 2;
+  text-decoration-color: #E87722;
+  transition: .3s;
+}
+</style>
+
+<div class="container">
+  <h1>uh oh...</h1>
+
+  <p>Something went wrong with your submission. Please try again.</p>
+
+  <p><a href="index.html">Go Back to Page</a></p>
+</div>
